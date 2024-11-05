f4582d9087803edd54082a3af464c0102f9a620d
pythonlearner12 <fhh1914@gmail.com>
2024-11-05 15:27:35 +0100

    opdracht 3 afgemaakt

---
+++ opdracht 3/main.js
@@ -0,0 +1,6 @@
+function autoFill() {
+    const userInput = document.getElementById('inputText').value;
+    document.getElementById('output1').value = userInput;
+    document.getElementById('output2').value = userInput;
+    document.getElementById('output3').value = userInput;
+}
